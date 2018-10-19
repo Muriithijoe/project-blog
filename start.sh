c5cbda5d9cbeae10abda4e2046920d6e4b49acc5
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-19 16:39:29 +0300

    start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME="joeace2000@gmail.com"
+export MAIL_PASSWORD="bankroll7028"
+export SECRET_KEY="123456789"
+export DATABASE_URL="postgresql+psycopg2://joe:killshot18@localhost/onepitch"
+
+python3.6 manage.py server
