f443b45102d70b47446af61731a2c3e49e350661
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-20 19:25:31 +0300

    models.py

--- app/models.py
+++ app/models.py
@@ -8,13 +8,40 @@ from . import login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
-    __tablename__= 'users'
+class Writer(UserMixin,db.Model):
+    __tablename__= 'writer'
     id=db.Column(db.Integer,primary_key =True)
     username = db.Column(db.String(255))
     email=db.Column(db.String(255),unique=True,index = True)
     bio =db.Column(db.String(255))
     profile_pic_path =db.Column(db.String())
+    pass_hash =db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password Attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_hash =generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_hash,password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+
+         return f'Writer {self.username}'
+
+
+class User(UserMixin,db.Model):
+    __tablename__= 'users'
+    id=db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255))
+    email=db.Column(db.String(255),unique=True,index = True)
     pass_hash =db.Column(db.String(255))
 
     @property
@@ -40,7 +67,7 @@ class Comment(db.Model):
     __tablename__ ='comments'
     id = db.Column(db.Integer,primary_key= True)
     comment=db.Column(db.String(255))
-    pitches=db.relationship('Pitch',backref='comments',lazy='dynamic')
+    blogs=db.relationship('Blogs',backref='comments',lazy='dynamic')
     user_id= db.Column(db.Integer,db.ForeignKey("users.id"))
 
     def save_comment(self):
