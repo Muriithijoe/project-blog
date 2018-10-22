cfb57edcbd6899d315b5a8f72991d194c20bde99
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-22 15:20:46 +0300

    did something

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from .forms import PostForm,SubscriberForm,CommentForm
+from .forms import BlogForm,SubscriberForm,CommentForm
 from ..import db,photos
 from ..models import User,Blog,Role,Subscriber,Comment
 from flask_login import login_required,current_user
