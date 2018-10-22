9c3b8eb815f76f82a2dbf7cce1ac3e7c8e77e07b
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-22 10:36:11 +0300

    new models

--- app/models.py
+++ app/models.py
@@ -2,107 +2,95 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class Writer(UserMixin,db.Model):
-    __tablename__= 'writer'
-    id=db.Column(db.Integer,primary_key =True)
-    username = db.Column(db.String(255))
-    email=db.Column(db.String(255),unique=True,index = True)
-    bio =db.Column(db.String(255))
-    profile_pic_path =db.Column(db.String())
-    pass_hash =db.Column(db.String(255))
-
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password Attribute')
-
-    @password.setter
-    def password(self,password):
-        self.pass_hash =generate_password_hash(password)
-
-    def verify_password(self,password):
-        return check_password_hash(self.pass_hash,password)
-
-    def save_user(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def __repr__(self):
-
-         return f'Writer {self.username}'
-
-
 class User(UserMixin,db.Model):
-    __tablename__= 'users'
-    id=db.Column(db.Integer,primary_key =True)
-    username = db.Column(db.String(255))
-    email=db.Column(db.String(255),unique=True,index = True)
-    pass_hash =db.Column(db.String(255))
+    __tablename__='users'
+
+    id=db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique=True,index=True)
+    password_hash = db.Column(db.String())
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     @property
     def password(self):
-        raise AttributeError('You cannot read the password Attribute')
+        raise AttributeError("You cannot read the password attribute")
 
     @password.setter
     def password(self,password):
-        self.pass_hash =generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_hash,password)
+        return check_password_hash(self.password_hash,password)
 
     def save_user(self):
         db.session.add(self)
         db.session.commit()
 
     def __repr__(self):
+        return f'User{self.username}'
 
-         return f'User {self.username}'
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Post(db.Model):
+    __tablename__='posts'
+
+    id = db.Column(db.Integer,primary_key=True)
+    post_id=db.Column(db.Integer)
+    title = db.Column(db.String)
+    post = db.Column(db.String)
+    category = db.Column(db.String)
+    like=db.Column(db.Integer)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_posts(cls,id):
+        posts=Post.query.filter_by(post_id=id).all()
+        return posts
 
 class Comment(db.Model):
-    __tablename__ ='comments'
-    id = db.Column(db.Integer,primary_key= True)
-    comment=db.Column(db.String(255))
-    blogs=db.relationship('Blogs',backref='comments',lazy='dynamic')
-    user_id= db.Column(db.Integer,db.ForeignKey("users.id"))
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    post_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
 
-    @classmethod
-    def get_comments(cls,id):
-        '''
-        Function that queries the Groups Table in the database and returns all the information from the Groups Table
-        Returns:
-            groups : all the information in the groups table
-        '''
-
-        comments = Comment.query.all()
-
-        return comments
-
-class Blog(db.Model):
-    __tablename__='pitches'
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(255))
-    description=db.Column(db.String)
-    like=db.Column(db.Integer)
-    dislike=db.Column(db.Integer)
-    comments_id =db.Column(db.Integer,db.ForeignKey("comments.id"))
-
-    def save_blog(self):
-        db.session.add(self)
+    def delete_comment(self):
+        db.session.delete(self)
         db.session.commit()
 
-    @classmethod
-    def get_blogs(cls,id):
-        '''
-        gets blogs from the database
-        '''
-        blogs=Blog.query.all()
-        return blogs
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+    users= db.relationship('User',backref='role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User{self.name}'
