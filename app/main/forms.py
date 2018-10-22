9152780def1b41d14da623e85453916aa1e863f1
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-22 12:25:54 +0300

    subscribe form

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,14 @@ class BlogForm(FlaskForm):
 class CommentForm(FlaskForm):
     comment = TextAreaField('Enter comment.',validators = [Required()])
     submit = SubmitField('Submit')
+class SubscriberForm(FlaskForm):
+    email = StringField("Email Address",validators=[Required(),Email()])
+    submit = SubmitField("Subscribe")
+
+    def validate_email(self,data_field):
+        if Subscriber.query.filter_by(email =data_field.data).first():
+            raise ValidationError("Account already subscribed with that email")
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import Writer,User,Comment,Blog
+from app.models import User,Comment,Blog
 
 
 app = create_app('production')
@@ -14,7 +14,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,Writer=Writer,User=User,Blog=Blog,Comment=Comment)
+    return dict(app = app,db = db,User=User,Blog=Blog,Comment=Comment)
 
 if __name__ == '__main__':
 
