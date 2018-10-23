6dc72cd5b333459dcb7d0c5a0a8a8b6440e6311a
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-10-23 10:51:24 +0300

    profile

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
 
         flash('Invalid Username or Password')
 
-    title ="Pitch Login"
+    title ="Blog Login"
     return render_template('auth/login.html',login_form=login_form,title=title)
 
 @auth.route('/logout')
